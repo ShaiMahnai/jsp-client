af7ee1d51ccd2672df3c69528932defb98d10c74
Shai Mahnai <shai.mahnai@gmail.com>
2020-11-16 20:25:16 +0200

    add try and catch to API calls

--- src/api/httpService.ts
+++ src/api/httpService.ts
@@ -6,14 +6,24 @@ const API_ADDRESS = 'http://127.0.0.1:5000';
 
 
 export const searchTransport = async (searchTransportRequest: SearchTransportRequest): Promise<SearchTransportResponse[]> => {
-    const res = await axios.post(`${API_ADDRESS}/search_tahbura`, searchTransportRequest);
-    console.log(res);
-    return res.data
+    try {
+        const res = await axios.post(`${API_ADDRESS}/search_tahbura`, searchTransportRequest);
+        console.log(res);
+        return res.data
+    } catch (e) {
+        console.log(`Exception: ${e}`);
+        return [];
+    }
 
 }
 export const searchPart = async (searchPartRequest: SearchPartRequest): Promise<SearchPartResponse> => {
-    const res = await axios.post(`${API_ADDRESS}/search_part_api`, searchPartRequest);
-    console.log(res);
-    return res.data
+    try {
+        const res = await axios.post(`${API_ADDRESS}/search_part_api`, searchPartRequest);
+        console.log(res);
+        return res.data
+    } catch (e) {
+        console.log(`Exception: ${e}`);
+        return {license_plate: searchPartRequest.license_plate, parts: [], vin: searchPartRequest.vin};
+    }
 
 }
